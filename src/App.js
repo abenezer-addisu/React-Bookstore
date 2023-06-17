efc09afd73cd3af7b80cbbfbdcf7477ca89c0686
abenezer-addisu <dasesatube@gmail.com>
2023-06-17 11:00:22 +0300

    project setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Book from './components/Book';
+import Nav from './components/Nav';
+import Categories from './components/Categories';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Nav />
+        <div className="app">
+          <Routes>
+            <Route index element={<Book />} />
+            <Route path="book" element={<Book />} />
+            <Route path="categories" element={<Categories />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
 
